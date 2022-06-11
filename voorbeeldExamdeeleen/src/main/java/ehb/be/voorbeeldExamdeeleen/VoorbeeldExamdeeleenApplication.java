d035a0405c6f2afff94091b6cf79b4267aa88831
Nassim Filali <nassim.filali@student.ehb.be>
2022-06-11 22:41:15 +0200

    Initial commit

---
+++ voorbeeldExamdeeleen/src/main/java/ehb/be/voorbeeldExamdeeleen/VoorbeeldExamdeeleenApplication.java
@@ -0,0 +1,13 @@
+package ehb.be.voorbeeldExamdeeleen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoorbeeldExamdeeleenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoorbeeldExamdeeleenApplication.class, args);
+	}
+
+}
---
+++ voorbeeldExamdeeleen/src/test/java/ehb/be/voorbeeldExamdeeleen/VoorbeeldExamdeeleenApplicationTests.java
@@ -0,0 +1,13 @@
+package ehb.be.voorbeeldExamdeeleen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoorbeeldExamdeeleenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
